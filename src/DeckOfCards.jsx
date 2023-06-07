88930cbf13ed23b9350fc09e642888fb0a7c35f6
pedrocordoba95 <pedrocordoba357@gmail.com>
2023-06-06 20:12:21 -0500

    myers se la traga toda, malparido

--- src/DeckOfCards.jsx
+++ src/DeckOfCards.jsx
@@ -1,6 +1,49 @@
-import React from 'react';
-import { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export function isCardUseful(hand, newCard) {
+  // Verificar si la nueva carta forma una terna, cuarta o escalera con las cartas existentes
+
+  // Obtener los valores y palos de las cartas en la mano del jugador
+  const values = hand.map(card => card.value);
+  const suits = hand.map(card => card.suit);
+
+  // Agregar el valor y palo de la nueva carta a las listas
+  values.push(newCard.value);
+  suits.push(newCard.suit);
+
+  function verificarTerna(values, elemento) {
+    const elementos = values.filter(value => value === elemento);
+    console.log()
+    return elementos.length === 3;
+  }
+
+  // Verificar si hay una cuarta o escalera
+  const uniqueValues = Array.from(new Set(values)); // Obtener los valores únicos
+  uniqueValues.sort(); // Ordenar los valores en orden ascendente
+
+  // Verificar si hay una escalera de al menos 4 cartas consecutivas
+  for (let i = 0; i < uniqueValues.length - 3; i++) {
+    const current = uniqueValues[i];
+    const nextThree = uniqueValues.slice(i + 1, i + 4);
+
+    if (nextThree.every((value, index) => value === current + (index + 1))) {
+      return true;
+    }
+  }
+
+  // Verificar si hay una cuarta
+  const fours = uniqueValues.filter(value => {
+    const count = values.filter(v => v === value).length;
+    return count === 4;
+  });
+
+  if (fours.length > 0) {
+    return true;
+  }
+
+  return false;
+}
 
 class DeckOfCards extends Component {
   constructor() {
@@ -8,25 +51,28 @@ class DeckOfCards extends Component {
     this.getCards = this.getCards.bind(this);
     this.giveMeCard = this.giveMeCard.bind(this);
     this.state = {
-      deck_id: "",
+      deck_id: '',
       imagesArray: [],
-      remaining: "",
+      remaining: '',
     };
   }
+
   getCards() {
-    axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/").then((data) =>
+    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/').then((data) =>
       this.setState({
         deck_id: data.data.deck_id,
         remaining: data.data.remaining,
       })
     );
   }
+
   componentDidMount() {
     this.getCards();
   }
+
   giveMeCard() {
     axios
-      .get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/`)
+      .get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=10`)
       .then((data) =>
         this.setState({
           imagesArray: [...this.state.imagesArray, data.data.cards[0].image],
@@ -35,69 +81,13 @@ class DeckOfCards extends Component {
       );
   }
 
-  export function isCardUseful(hand, newCard) {
-    // Verificar si la nueva carta forma una terna, cuarta o escalera con las cartas existentes
-  
-    // Obtener los valores y palos de las cartas en la mano del jugador
-    const values = hand.map(card => card.value);
-    const suits = hand.map(card => card.suit);
-  
-    // Agregar el valor y palo de la nueva carta a las listas
-    values.push(newCard.value);
-    suits.push(newCard.suit);
-  
-    // Verificar si hay una terna de 7s
-    const sevens = values.filter(value => value === '7');
-    if (sevens.length === 3) {
-      return true;
-    }
-  
-    // Verificar si hay una terna de 2s
-    const twos = values.filter(value => value === '2');
-    if (twos.length === 3) {
-      return true;
-    }
-  
-    // Verificar si hay una cuarta o escalera
-    const uniqueValues = Array.from(new Set(values)); // Obtener los valores únicos
-    uniqueValues.sort(); // Ordenar los valores en orden ascendente
-  
-    // Verificar si hay una escalera de al menos 4 cartas consecutivas
-    for (let i = 0; i < uniqueValues.length - 3; i++) {
-      const current = uniqueValues[i];
-      const nextThree = uniqueValues.slice(i + 1, i + 4);
-  
-      if (nextThree.every((value, index) => value === current + (index + 1))) {
-        return true;
-      }
-    }
-  
-    // Verificar si hay una cuarta
-    const fours = uniqueValues.filter(value => {
-      const count = values.filter(v => v === value).length;
-      return count === 4;
-    });
-  
-    if (fours.length > 0) {
-      return true;
-    }
-  
-    return false;
-  }
-  
-  // Uso de la función en un componente de función
-  // export default function CardGame() {
-  //   // Lógica del juego de cartas y componentes JSX aquí
-  //   // ...
-  // }
-
   render() {
     return (
       <div className="deck-board">
         {this.state.remaining ? (
           <button onClick={this.giveMeCard}>Give me a card</button>
         ) : (
-          ""
+          ''
         )}
         <div className="card-list">
           {this.state.imagesArray.map((card, index) => {
