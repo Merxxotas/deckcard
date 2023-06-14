d0c71c8140f3cbf209ec37cdcda7e4da674eaebc
Merxxotas <brayan6590@gmail.com>
2023-06-13 20:49:09 -0500

    Pedro la chupa demasiado duro, que asco

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
-React App juego de cartas
-=======================
+# React App juego de cartas
 
 Este proyecto es usado mediante las tecnologias de REACT y el consumo de la siguiente API: [deckofcardsapi.com](https://deckofcardsapi.com/) Permitiendo a los usuarios traer de a una carta para su propio beneficio.
 
-Tecnologías
-------------
+## Tecnologías
 
--   React
--   deckofcardsapi.com API
--   
-Se ejecunta usando:
-------------
+- React
+- deckofcardsapi.com API
+- Se ejecuta usando:
+
+---
 
 1.  Clonar el repositorio
 2.  ir hasta la carpeta: `cd deck-cards`
 3.  Instalar Dependencias: `npm install`
 4.  Iniciar la APP: `npm start`
 
-Programación Distribuida y paralela
--------
+## Programación Distribuida y paralela
 
--   Brayan Marín Guirales
--   Pedro Hernán Cordoba Diosa
+- Brayan Marín Guirales
+- Pedro Hernán Cordoba Diosa
 
-Politécnico Colombiano Jaime Isaza Cadavid 2023-1
--------
+## Politécnico Colombiano Jaime Isaza Cadavid 2023-1
 
 No mirar los nombres de los commits, son para mero objetivo de entretener (?)
--- src/game.jsx
+++ src/game.jsx
@@ -137,6 +137,7 @@
 
 import React, { useState } from "react";
 import { Button, TextField, Grid, Typography } from "@mui/material";
+import Notify, { AlertTypes } from "./services/Notify";
 
 const App = () => {
   const [deckId, setDeckId] = useState("");
@@ -181,84 +182,130 @@ const App = () => {
     }
   };
 
-// ...
+  // ...
 
-const drawCard = async () => {
-  try {
-    // Obtener una carta para cada jugador
-    const player1Response = await fetch(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-    );
-    const player1Data = await player1Response.json();
-    const player1Card = player1Data.cards[0];
+  // ...
 
-    const player2Response = await fetch(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-    );
-    const player2Data = await player2Response.json();
-    const player2Card = player2Data.cards[0];
-
-    // Verificar si el jugador 1 necesita descartar una carta
-    let newPlayer1Cards = player1Cards;
-    if (player1Cards.length === 10) {
-      const cardToDiscard = player1Cards.find((card) =>
-        !checkCardValue(card, [...player1Cards, player1Card], player1Name)
+  const drawCard = async () => {
+    try {
+      // Obtener una carta para cada jugador
+      const player1Response = await fetch(
+        `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
       );
-      if (cardToDiscard) {
-        newPlayer1Cards = player1Cards.filter((card) => card !== cardToDiscard);
-        setPlayer1DiscardedCards((prevCards) => [...prevCards, cardToDiscard]);
-      }
-    }
+      const player1Data = await player1Response.json();
+      const player1Card = player1Data.cards[0];
 
-    // Verificar si el jugador 2 necesita descartar una carta
-    let newPlayer2Cards = player2Cards;
-    if (player2Cards.length === 10) {
-      const cardToDiscard = player2Cards.find((card) =>
-        !checkCardValue(card, [...player2Cards, player2Card], player2Name)
+      const player2Response = await fetch(
+        `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
       );
-      if (cardToDiscard) {
-        newPlayer2Cards = player2Cards.filter((card) => card !== cardToDiscard);
-        setPlayer2DiscardedCards((prevCards) => [...prevCards, cardToDiscard]);
+      const player2Data = await player2Response.json();
+      const player2Card = player2Data.cards[0];
+
+      // Verificar si el mazo se quedó sin cartas
+      if (player1Data.remaining === 0 || player2Data.remaining === 0) {
+        Notify.sendNotification("El mazo se quedó sin cartas", AlertTypes.warn);
+
+        // Obtener un nuevo mazo barajado
+        const shuffleResponse = await fetch(
+          "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+        );
+        const shuffleData = await shuffleResponse.json();
+        const newDeckId = shuffleData.deck_id;
+        setDeckId(newDeckId);
+
+        // Extraer las cartas barajadas para el jugador 1
+        const player1NewResponse = await fetch(
+          `https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=10`
+        );
+        const player1NewData = await player1NewResponse.json();
+        const player1NewDrawnCards = player1NewData.cards;
+        setPlayer1Cards(player1NewDrawnCards);
+
+        // Extraer las cartas barajadas para el jugador 2
+        const player2NewResponse = await fetch(
+          `https://deckofcardsapi.com/api/deck/${newDeckId}/draw/?count=10`
+        );
+        const player2NewData = await player2NewResponse.json();
+        const player2NewDrawnCards = player2NewData.cards;
+        setPlayer2Cards(player2NewDrawnCards);
+
+        setGameStarted(true);
+        return;
       }
+
+      // Verificar si el jugador 1 necesita descartar una carta
+      let newPlayer1Cards = player1Cards;
+      if (player1Cards.length === 10) {
+        const cardToDiscard = player1Cards.find(
+          (card) =>
+            !checkCardValue(card, [...player1Cards, player1Card], player1Name)
+        );
+        if (cardToDiscard) {
+          newPlayer1Cards = player1Cards.filter(
+            (card) => card !== cardToDiscard
+          );
+          setPlayer1DiscardedCards((prevCards) => [
+            ...prevCards,
+            cardToDiscard,
+          ]);
+        }
+      }
+
+      // Verificar si el jugador 2 necesita descartar una carta
+      let newPlayer2Cards = player2Cards;
+      if (player2Cards.length === 10) {
+        const cardToDiscard = player2Cards.find(
+          (card) =>
+            !checkCardValue(card, [...player2Cards, player2Card], player2Name)
+        );
+        if (cardToDiscard) {
+          newPlayer2Cards = player2Cards.filter(
+            (card) => card !== cardToDiscard
+          );
+          setPlayer2DiscardedCards((prevCards) => [
+            ...prevCards,
+            cardToDiscard,
+          ]);
+        }
+      }
+
+      // Actualizar las cartas de los jugadores
+      setPlayer1Cards((prevCards) => [...newPlayer1Cards, player1Card]);
+      setPlayer2Cards((prevCards) => [...newPlayer2Cards, player2Card]);
+
+      // Verificar coincidencias de valor
+      checkCardValue(player1Card, newPlayer1Cards, player1Name);
+      checkCardValue(player2Card, newPlayer2Cards, player2Name);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    // Actualizar las cartas de los jugadores
-    setPlayer1Cards((prevCards) => [...newPlayer1Cards, player1Card]);
-    setPlayer2Cards((prevCards) => [...newPlayer2Cards, player2Card]);
-
-    // Verificar coincidencias de valor
-    checkCardValue(player1Card, newPlayer1Cards, player1Name);
-    checkCardValue(player2Card, newPlayer2Cards, player2Name);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const checkCardValue = (newCard, playerCards, playerName) => {
-  const cardsWithSameValue = playerCards.filter(
-    (card) => card.value === newCard.value
-  );
-
-  if (cardsWithSameValue.length >= 4) {
-    console.log(`¡${playerName} tiene 4 cartas del mismo valor!`);
-    return true;
-  }
-
-  if (cardsWithSameValue.length === 3) {
-    const remainingCards = playerCards.filter(
-      (card) => card.value !== newCard.value
+  const checkCardValue = (newCard, playerCards, playerName) => {
+    const cardsWithSameValue = playerCards.filter(
+      (card) => card.value === newCard.value
     );
-    const remainingValues = remainingCards.map((card) => card.value);
-    const remainingValueSet = new Set(remainingValues);
 
-    if (!remainingValueSet.has(newCard.value)) {
-      console.log(`¡${playerName} tiene 3 cartas del mismo valor!`);
+    if (cardsWithSameValue.length >= 4) {
+      console.log(`¡${playerName} tiene 4 cartas del mismo valor!`);
       return true;
     }
-  }
 
-  return false;
-};
+    if (cardsWithSameValue.length === 3) {
+      const remainingCards = playerCards.filter(
+        (card) => card.value !== newCard.value
+      );
+      const remainingValues = remainingCards.map((card) => card.value);
+      const remainingValueSet = new Set(remainingValues);
+
+      if (!remainingValueSet.has(newCard.value)) {
+        console.log(`¡${playerName} tiene 3 cartas del mismo valor!`);
+        return true;
+      }
+    }
+
+    return false;
+  };
 
   return (
     <div>
@@ -293,9 +340,9 @@ const checkCardValue = (newCard, playerCards, playerName) => {
           </Button>
           <Typography variant="h5">{player1Name}</Typography>
           <div style={{ display: "flex" }}>
-            {player1Cards.map((card) => (
+            {player1Cards.map((card, index) => (
               <img
-                key={card.code}
+                key={`player1_${index}_${card.code}`}
                 src={card.image}
                 alt={card.code}
                 style={{ marginRight: "10px", width: "100px" }}
@@ -304,9 +351,9 @@ const checkCardValue = (newCard, playerCards, playerName) => {
           </div>
           <Typography variant="h5">{player2Name}</Typography>
           <div style={{ display: "flex" }}>
-            {player2Cards.map((card) => (
+            {player2Cards.map((card, index) => (
               <img
-                key={card.code}
+                key={`player2_${index}_${card.code}`}
                 src={card.image}
                 alt={card.code}
                 style={{ marginRight: "10px", width: "100px" }}
@@ -331,4 +378,3 @@ const checkCardValue = (newCard, playerCards, playerName) => {
 };
 
 export default App;
-
